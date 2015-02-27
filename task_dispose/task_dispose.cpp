ca5100faaae9d855b57159641d90ce344bc5c745
duoniduoni <duoniduoni@126.com>
2015-02-27 17:24:35 +0800

    正在处理字符识别
    将字符像素点获取出来

--- task_dispose/task_dispose.cpp
+++ task_dispose/task_dispose.cpp
@@ -58,6 +58,41 @@ void getLines(Mat & src, vector<int> & lines)
 	}
 }
 
+void getCharacterPoints(Mat & src, vector<CvPoint> & points)
+{
+	for(int x = 0; x < src.rows; x++)
+	{
+		uchar *p = src.ptr<uchar>(x);
+		//行内扫描
+		bool flag = false;
+		for(int y = 0; y < src.cols; ++y)
+		{
+			if(flag == false) //未找到开始点
+			{
+				if(y + 1 < src.cols)
+				{
+					if( p[y] == 255 && p[y + 1] == 0)
+					{
+						flag = true;
+					}
+				}
+			}
+			else
+			{
+				points.push_back(cvPoint(x, y));
+				if(y + 1 < src.cols)
+				{
+					if(p[y] == 0 && p[y + 1] == 255)
+					{
+						flag = false;
+						y += 2;
+					}
+				}
+			}
+		}
+	}
+}
+
 int _tmain(int argc, char * argv[])
 {
 	char * path = NULL;
@@ -81,13 +116,14 @@ int _tmain(int argc, char * argv[])
 
 	//二值化处理 将大于0的
 	Mat dst = src.clone();
-	threshold(src, dst, 1, 255, THRESH_BINARY_INV);
+	threshold(src, dst, 3, 255, THRESH_BINARY_INV);
 	imshow(WNAME, dst);
 
 	cout << "press any key to into next step !" << endl;
 	cvWaitKey(0);
 
 	//找行
+	/*
 	vector<int> lines;
 	getLines(dst, lines);
 
@@ -95,6 +131,51 @@ int _tmain(int argc, char * argv[])
 	for(int i = 0; i < (int)lines.size(); i++)
 		line(dst, cvPoint(0, lines[i]), cvPoint(dst.cols, lines[i]), cvScalar(255, 255, 255));
 	imshow(WNAME, dst);
+
+	cout << "press any key to into next step !" << endl;
+	cvWaitKey(0);
+	*/
+
+	//初步找字符的点
+	vector<CvPoint> points;
+	getCharacterPoints(dst, points);
+	cout << "points.size = " << points.size() << endl;
+
+	//显示字符点查找结果
+	Mat show = Mat::zeros(dst.rows, dst.cols, CV_8UC3);
+
+	for(int x = 0; x < show.rows; ++x)
+	{
+		Vec3b * n = show.ptr<Vec3b>(x);
+		uchar * o = dst.ptr<uchar>(x);
+
+		for(int y = 0; y < show.cols; ++y)
+		{
+			if(o[y] == 255)
+				n[y][0] = n[y][1] = n[y][2] = 255;
+		}
+	}
+	/*
+	Vec3b * p = show.ptr<Vec3b>(0);
+	int curline = 0;
+	for(int i = 0; i < points.size(); i++)
+	{
+		if(points[i].x != curline)
+		{
+			curline = points[i].x;
+			p = show.ptr<Vec3b>(curline);
+		}
+
+		p[points[i].y][2] = 0;
+		p[points[i].y][1] = 0;
+		p[points[i].y][0] = 0;
+		p[points[i].y][rand()%3] = 255;
+	}*/
+
+	Mat show2 = Mat::zeros(dst.rows, dst.cols, CV_8UC3);
+	bitwise_not(show, show2);
+	imshow(WNAME, show2);
+	imwrite("resutl.bmp", show2);
 
 	cout << "press any key to into next step !" << endl;
 	cvWaitKey(0);
