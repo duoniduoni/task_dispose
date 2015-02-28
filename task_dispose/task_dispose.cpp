428ab7ad41d1e3e82518c4cbc71472ab2663f034
duoniduoni <duoniduoni@126.com>
2015-02-28 17:04:18 +0800

    依然在处理字符识别
    使用轮廓统计出来了行的信息，但是感觉没啥大用

--- task_dispose/task_dispose.cpp
+++ task_dispose/task_dispose.cpp
@@ -14,83 +14,27 @@ using namespace cv;
 
 #define WNAME "window_for_show"
 
-bool isLineEmpty(uchar * p, int len)
-{
-	for(int i = 0; i < len; i++)
-		if(p[i] != 0)
-			return false;
 
-	return true;
-}
-
-void getLines(Mat & src, vector<int> & lines)
+CvRect getContourRect(vector<Point> & contour)
 {
-	if(src.channels() != 1)
+	int top = contour[0].y, left = contour[0].x, right = 0, bottom = 0;
+
+	for(int i = 0; i < contour.size(); ++i)
 	{
-		lines.clear();
-		return ;
+		if(contour[i].x < left)
+			left = contour[i].x;
+
+		if(contour[i].x > right)
+			right = contour[i].x;
+
+		if(contour[i].y < top)
+			top = contour[i].y;
+
+		if(contour[i].y > bottom)
+			bottom = contour[i].y;
 	}
 
-	bool find_begin = false, find_end = false;
-
-	for(int i = 0; i < src.rows; i++)
-	{
-		uchar *p = src.ptr<uchar>(i);
-		if(find_begin == false)
-		{
-			if(!isLineEmpty(p, src.cols))
-			{
-				find_begin = true;
-				lines.push_back(i);
-			}
-		}
-		else
-		{
-			if(isLineEmpty(p, src.cols))
-			{
-				find_end = true;
-				lines.push_back(i);
-			}
-		}
-
-		if(find_begin == true && find_end == true)
-			find_begin = find_end = false;
-	}
-}
-
-void getCharacterPoints(Mat & src, vector<CvPoint> & points)
-{
-	for(int x = 0; x < src.rows; x++)
-	{
-		uchar *p = src.ptr<uchar>(x);
-		//行内扫描
-		bool flag = false;
-		for(int y = 0; y < src.cols; ++y)
-		{
-			if(flag == false) //未找到开始点
-			{
-				if(y + 1 < src.cols)
-				{
-					if( p[y] == 255 && p[y + 1] == 0)
-					{
-						flag = true;
-					}
-				}
-			}
-			else
-			{
-				points.push_back(cvPoint(x, y));
-				if(y + 1 < src.cols)
-				{
-					if(p[y] == 0 && p[y + 1] == 255)
-					{
-						flag = false;
-						y += 2;
-					}
-				}
-			}
-		}
-	}
+	return cvRect(left, top, right - left, bottom - top);
 }
 
 int _tmain(int argc, char * argv[])
@@ -122,60 +66,38 @@ int _tmain(int argc, char * argv[])
 	cout << "press any key to into next step !" << endl;
 	cvWaitKey(0);
 
-	//找行
-	/*
-	vector<int> lines;
-	getLines(dst, lines);
+	//取得二值化图片中所有特征外部轮廓
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	findContours( dst, contours, hierarchy, CV_RETR_EXTERNAL/*CV_RETR_TREE*/, CV_CHAIN_APPROX_NONE );
 
-	cout << "lines.size() = " << lines.size() << endl;
-	for(int i = 0; i < (int)lines.size(); i++)
-		line(dst, cvPoint(0, lines[i]), cvPoint(dst.cols, lines[i]), cvScalar(255, 255, 255));
-	imshow(WNAME, dst);
+	//对轮廓进行统计
+	vector<int> heights, pos;
+	for( int i = 0; i< contours.size(); i++ )
+    {
+		//计算轮廓面积 舍弃过小的
+		double area = contourArea(contours[i]);
+		if(area < 20)
+			continue;
 
-	cout << "press any key to into next step !" << endl;
-	cvWaitKey(0);
-	*/
+		//获取轮廓矩形
+		CvRect rect = getContourRect(contours[i]);
 
-	//初步找字符的点
-	vector<CvPoint> points;
-	getCharacterPoints(dst, points);
-	cout << "points.size = " << points.size() << endl;
+		//记录高度
+		heights.push_back(rect.height);
+		//记录行中央位置
+		pos.push_back(rect.y + rect.height/2);
+    }
 
-	//显示字符点查找结果
-	Mat show = Mat::zeros(dst.rows, dst.cols, CV_8UC3);
+	sort(heights.begin(), heights.end());
+	sort(pos.begin(), pos.end());
 
-	for(int x = 0; x < show.rows; ++x)
-	{
-		Vec3b * n = show.ptr<Vec3b>(x);
-		uchar * o = dst.ptr<uchar>(x);
+	int i;
+	for(i = 0; i < heights.size(); ++i)
+		cout << heights[i] << endl;
 
-		for(int y = 0; y < show.cols; ++y)
-		{
-			if(o[y] == 255)
-				n[y][0] = n[y][1] = n[y][2] = 255;
-		}
-	}
-	/*
-	Vec3b * p = show.ptr<Vec3b>(0);
-	int curline = 0;
-	for(int i = 0; i < points.size(); i++)
-	{
-		if(points[i].x != curline)
-		{
-			curline = points[i].x;
-			p = show.ptr<Vec3b>(curline);
-		}
-
-		p[points[i].y][2] = 0;
-		p[points[i].y][1] = 0;
-		p[points[i].y][0] = 0;
-		p[points[i].y][rand()%3] = 255;
-	}*/
-
-	Mat show2 = Mat::zeros(dst.rows, dst.cols, CV_8UC3);
-	bitwise_not(show, show2);
-	imshow(WNAME, show2);
-	imwrite("resutl.bmp", show2);
+	for(i = 0; i < pos.size(); ++i)
+		cout << pos[i] << endl;
 
 	cout << "press any key to into next step !" << endl;
 	cvWaitKey(0);
